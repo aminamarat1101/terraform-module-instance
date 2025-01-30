5ff33e7c4874b47ec3e58c7c351196165fad83db
aminamarat1101 <aminamaratt@gmail.com>
2025-01-30 08:21:36 +0000

    added

--- Homework6/main.tf
+++ Homework6/main.tf
@@ -1,9 +1,9 @@
-provider aws {
-    region = "us-east-1"
+provider "aws" {
+  region = "us-east-1"
 }
 
 resource "aws_instance" "web" {
-  ami           = "ami-0c614dee691cbbf37"  # Amazon Linux AMI
+  ami           = "ami-0c614dee691cbbf37" # Amazon Linux AMI
   instance_type = var.instance_type
 
   tags = {
