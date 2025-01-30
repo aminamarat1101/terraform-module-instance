1852cc22e301cf1c936f5bfa03953a93212bf695
aminamarat1101 <aminamaratt@gmail.com>
2025-01-30 08:00:34 +0000

    files added

---
+++ Homework6/main.tf
@@ -0,0 +1,14 @@
+provider aws {
+    region = "us-east-1"
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-0c614dee691cbbf37"  # Amazon Linux AMI
+  instance_type = var.instance_type
+
+  tags = {
+    Name = var.instance_name
+  }
+
+  user_data = file("apache.sh")
+}
