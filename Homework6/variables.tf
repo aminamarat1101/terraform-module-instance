c6f461b5a124c4c57a232977e03ec9b0c673da4c
aminamarat1101 <aminamaratt@gmail.com>
2025-01-30 08:10:28 +0000

    added

--- Homework6/variables.tf
+++ Homework6/variables.tf
@@ -1,2 +1,9 @@
-variable instance_type {}
-variable instance_name {}
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+}
+
+variable "instance_name" {
+  description = "Name of the EC2 instance"
+  type        = string
+}
